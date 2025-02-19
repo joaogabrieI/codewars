16341183a886a3e6e08b52a409123dac98f0f464
joaogabrieI <joaonalves1@gmail.com>
2025-02-19 16:47:08 -0300

    'Name Shuffler' kata solution

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        // int[] numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+        // foreach (var num in CountPositivesSumNegatives(numeros))
+        // {
+        //     Console.WriteLine(num);
+        // }
+        // Console.WriteLine(Greet("João"));
+
+        double[] amostras = new double[5];
+        amostras.SetValue(8.3, 0);
+        amostras.SetValue(7.0, 1);
+        amostras.SetValue(3.4, 2);
+        amostras.SetValue(5.2, 3);
+        amostras.SetValue(9.7, 4);
+
+        Media(amostras);
+
+        // System.Console.WriteLine(dnaToRna("GCAT"));
+        System.Console.WriteLine(NameShuffler("john McClane"));
+    }
+
+    public static string Greet(string name)
+    {
+        return $"Hello, {name} how are you doing today?";
+    }
+
+    public static int[] CountPositivesSumNegatives(int[] input)
+    {
+        int[] output = [];
+        int negatives = 0;
+        int positives = 0;
+        if (input != null)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] > 0)
+                {
+                    positives += 1;
+                }
+
+                if (input[i] < 0)
+                {
+                    negatives += input[i];
+                }
+                output = [positives, negatives];
+            }
+        }
+        return output;
+    }
+
+    static void Media(double[] amostras)
+    {
+        double soma = 0;
+        for (int i = 0; i < amostras.Length; i++)
+        {
+            soma += Convert.ToDouble(amostras.GetValue(i));
+        }
+        Console.WriteLine($"Média: {(soma / amostras.Length):N2}");
+    }
+
+    public static string NameShuffler(string str)
+    {
+        int divisor = 0;
+        string primeiraParte = "";
+        string segundaParte = "";
+        for(int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == ' ')
+            {
+                divisor = i;
+            }
+        }
+
+        for(int j = 0; j < divisor; j++)
+        {
+            primeiraParte += str[j];
+        }
+
+        for(int k = divisor+1; k < str.Length; k++)
+        {
+            segundaParte += str[k];
+        }    
+
+        return segundaParte + " " + primeiraParte;
+    }
+}
